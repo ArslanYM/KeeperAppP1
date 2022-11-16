94cf66878cd9068bd030b8215d7b9729878539ac
ArslanYM <104521101+ArslanYM@users.noreply.github.com>
2022-11-16 18:32:11 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
